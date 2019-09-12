c5b765b51939a39f8ce412314fae6692108d18d6
Wasylews <ua.wasylews@gmail.com>
2019-09-12 16:17:21 +0300

    Add model module

---
+++ model/src/main/java/com/genius/wasylews/model/Exhibit.kt
@@ -0,0 +1,6 @@
+package com.genius.wasylews.model
+
+data class Exhibit(
+    val title: String,
+    val images: List<String>
+)
---
+++ model/src/main/java/com/genius/wasylews/model/ExhibitLoader.kt
@@ -0,0 +1,6 @@
+package com.genius.wasylews.model
+
+interface ExhibitLoader {
+
+    fun getExhibitList():List<Exhibit>
+}
