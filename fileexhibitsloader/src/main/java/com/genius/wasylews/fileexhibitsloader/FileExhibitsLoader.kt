b624ac85b4f73c07d377c32ced4e1267e1aa4d5a
Wasylews <ua.wasylews@gmail.com>
2019-09-12 16:20:07 +0300

    Implement loading json from assets

---
+++ fileexhibitsloader/src/main/java/com/genius/wasylews/fileexhibitsloader/FileExhibitsLoader.kt
@@ -0,0 +1,21 @@
+package com.genius.wasylews.fileexhibitsloader
+
+import android.content.Context
+import com.genius.wasylews.model.Exhibit
+import com.genius.wasylews.model.ExhibitLoader
+import com.google.gson.Gson
+
+class FileExhibitsLoader(private val context: Context,
+                         private val fileName: String): ExhibitLoader {
+
+    private val gson = Gson()
+
+    override fun getExhibitList(): List<Exhibit> {
+        val reader = context.assets
+            .open(fileName)
+            .bufferedReader()
+
+        val json = reader.use { it.readText() }
+        return gson.fromJson(json, Array<Exhibit>::class.java).asList()
+    }
+}
